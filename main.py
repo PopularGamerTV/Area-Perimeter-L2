6d123032770254857cadc9d37c3821cac131aa7b
Joshua Harvey <>
2021-04-13 22:25:28 +0000

    This code shows the user what shapes that they can use to calculate the perimeter and the area of one of the 6 shapes. A problem I had was having a space between the code asking stating what number goes to what shape and asking for the input

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Chose a shape to calculate the area and perimeter of. #1: Circle, #2: Square, #3: Rectangle, #4: Triangle, #5: Parrelelogram, #6: Trapezium\n")
+
+shapechoice = input("Please select number for shape to find area and perimeter:")
+shapechoice = int(shapechoice)
