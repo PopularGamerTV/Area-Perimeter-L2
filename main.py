bb262274fbe5a67d63de64e9de03be03e7d1a947
Joshua Harvey <>
2021-04-13 22:39:13 +0000

    This part of the code when chosen either shape 1 or 2 will bring up the inputs that are needed to solve the perimeter and area of selected shape

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ print("Chose a shape to calculate the area and perimeter of. #1: Circle, #2: Squ
 
 shapechoice = input("Please select number for shape to find area and perimeter:")
 shapechoice = int(shapechoice)
+
+if shapechoice == 1:
+  r=float(input("Input Radius : "))
+  carea=3.14*r*r
+  cperemiter=2*3.14*r
+  print("Area of the circle: ",carea)
+  print("Perimeter of the circle: ",cperemiter)
+
+if shapechoice == 2:
+  l=float(input("Input Length : "))
+  w=float(input("Input Width : "))
+  sarea=l*w
+  sperimeter=2*(l+w)
+  print("Area of a square: ",sarea)
+  print("Peremiter of a square: ",sperimeter)
