f33a97385f3ba2756d49dfdc4903bf9c21b586cb
Joshua Harvey <>
2021-05-11 22:55:26 +0000

    Teacher has helped me make the code repeat itself and made my code able to shorten  itself while using 'def'

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-print("Chose a shape to calculate the area and perimeter of. #1: Circle, #2: Square, #3: Rectangle, #4: Triangle, #5: Parrelelogram, #6: Trapezium\n")
+print("Area/Perimeter Calculator")
 
-shapechoice = input("Please select number for shape to find area and perimeter:")
-shapechoice = int(shapechoice)
-
-if shapechoice == 1:
+def circle():
   r=float(input("Input Radius : "))
   carea=3.14*r*r
   cperemiter=2*3.14*r
   print("Area of the circle: ",carea)
   print("Perimeter of the circle: ",cperemiter)
 
-if shapechoice == 2:
+def square():
   l=float(input("Input Length : "))
   w=float(input("Input Width : "))
   sarea=l*w
@@ -18,7 +15,7 @@ if shapechoice == 2:
   print("Area of a square: ",sarea)
   print("Peremiter of a square: ",sperimeter)
 
-if shapechoice == 3:
+def rectangle():
   l=float(input("Input Length : "))
   w=float(input("Input Width : "))
   rarea=l*w
@@ -26,7 +23,7 @@ if shapechoice == 3:
   print("Area of a square: ",rarea)
   print("Peremiter of a square: ",rperimeter)
 
-if shapechoice == 4:
+def triangle():
   b=float(input("Input Base : "))
   h=float(input("Input height : "))
   a=float(input("Input Side 1 : "))
@@ -34,10 +31,10 @@ if shapechoice == 4:
   c=float(input("Input Side 3 : "))
   triarea=0.5*b*h
   triperimeter=a+b+c
-  print("Area of a square: ",triarea)
-  print("Perimeter of a square: ",triperimeter)
+  print("Area of a triangle: ",triarea)
+  print("Perimeter of a triangle: ",triperimeter)
 
-if shapechoice == 5:
+def parrelelogram():
   l=float(input("Input Length : "))
   w=float(input("Input Width : "))
   parea=l*w
@@ -45,7 +42,7 @@ if shapechoice == 5:
   print("Area of a parrelelogram: ",parea)
   print("Perimeter of a parrelelogram: ",pperimeter)
 
-if shapechoice == 6:
+def trapezium():
   b=float(input("Input Base : "))
   t=float(input("Input Top : "))
   h=float(input("Input Height : "))
@@ -55,3 +52,36 @@ if shapechoice == 6:
   trapperimeter=b+t+a+c
   print("Area of a trapizium: ",traparea)
   print("Perimeter of a trapizium: ",trapperimeter)
+
+def choice(shapechoice):
+  if shapechoice == 1:
+    circle()
+
+  if shapechoice == 2:
+    square()
+
+  if shapechoice == 3:
+    rectangle()
+
+  if shapechoice == 4:
+    triangle()
+
+  if shapechoice == 5:
+    parrelelogram()
+
+  if shapechoice == 6:
+   trapezium()
+
+keep_going = ""
+while keep_going == "":
+
+  print("Chose a shape to calculate the area and perimeter of. #1: Circle, #2: Square, #3: Rectangle, #4: Triangle, #5: Parrelelogram, #6: Trapezium\n")
+
+  shapechoice = input("Please select number for shape to find area and perimeter:")
+  shapechoice = int(shapechoice)
+
+  choice(shapechoice)
+  
+  keep_going=input("\nPress 'Enter' to continue or 'Any key' to quit>>")
+
+print("Thank you for using this calculator. Hope to see you again!")
